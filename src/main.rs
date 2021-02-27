1b8c79a4ae8062d2a8eca636c782f7c5bda79813
Blinningjr <nicke.l@telia.com>
2021-02-27 20:45:56 +0100

    Added more for testeing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 //! main.rs
 
-#![deny(unsafe_code)]
+//#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
- 
+#![feature(asm, test)]
+
+
+use cortex_m::asm;
 use panic_halt as _;
 use rtt_target::{rprintln, rtt_init_print};
 use stm32f4xx_hal::{
@@ -11,8 +14,23 @@ use stm32f4xx_hal::{
     gpio::*,
 };
 use rtic::cyccnt::{Instant, U32Ext};
+use core::hint::black_box;
 
 const PERIOD: u32 = 8_000_000;
+
+#[derive(Clone, Debug)]
+enum TestEnum {
+    ITest(i32),
+    UTest(u32),
+    Struct(TestStruct),
+    Non,
+}
+
+#[derive(Clone, Debug)]
+struct TestStruct {
+    flag: bool,
+    num: u32,
+}
 
 #[rtic::app(device = stm32f4xx_hal::stm32, monotonic = rtic::cyccnt::CYCCNT, peripherals = true)]
 const APP: () = {
@@ -40,18 +58,51 @@ const APP: () = {
         }
     }
     
+//    #[no_mangle]
+//    #[inline(never)]
     #[task(schedule = [led_off], resources = [led])]
     fn led_on(cx: led_on::Context) {
-        rprintln!("led_on");
+        let mut my_num = 10;
+        let mut test_enum1 = TestEnum::ITest(20);
+        let mut test_enum2 = TestEnum::Non;
+        let mut test_struct = TestStruct{
+            flag: true,
+            num: 123,
+        };
+        let mut  test_enum3 = TestEnum::Struct(test_struct.clone());
+        
+        my_num = black_box(my_num);
+        test_enum1 = black_box(test_enum1);
+        test_enum2 = black_box(test_enum2);
+        test_enum3 = black_box(test_enum3);
+        test_struct = black_box(test_struct);
+//        black_box(&cx);
+
         cx.schedule.led_off(cx.scheduled + PERIOD.cycles()).unwrap();
         cx.resources.led.set_high().unwrap();
+
+        unsafe{asm!{"bkpt"};}  //asm::bkpt();
+
+//        black_box(&cx);
+        black_box(my_num);
+        black_box(test_enum1);
+        black_box(test_enum2);
+        black_box(test_enum3);
+        black_box(test_struct);
     }
 
     #[task(schedule = [led_on], resources = [led])]
     fn led_off(cx: led_off::Context) {
-        rprintln!("led_off");
+        let my_num = 30;
+        let test_enum = TestEnum::UTest(40);
+
         cx.schedule.led_on(cx.scheduled + PERIOD.cycles()).unwrap();
         cx.resources.led.set_low().unwrap();
+        
+//        unsafe{asm!{"bkpt"};}  //asm::bkpt();
+
+        //black_box(my_num);
+        //black_box(test_enum);
     }
 
     extern "C" {
